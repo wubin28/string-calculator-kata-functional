f20a8510d926d526d6582357ad2366d81970e089
Ben Wu <wubin28@gmail.com>
2016-03-10 12:56:52 +0800

    Wrote a dummy test.

---
+++ stringcalculator/src/test/java/kata/functional/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package kata.functional;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StringCalculatorTest {
+    @Test
+    public void should() {
+        assertEquals(1, 1);
+    }
+}
