8b3cf719ccaffe66dba03d9a6ffa0f710b0ba6fa
Ben Wu <wubin28@gmail.com>
2016-03-10 14:07:44 +0800

    Wrote two filters.

--- stringcalculator/src/main/java/kata/functional/StringCalculator.java
+++ stringcalculator/src/main/java/kata/functional/StringCalculator.java
@@ -1,10 +1,36 @@
 package kata.functional;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
 /**
  * Created by twer on 3/10/16.
  */
 public class StringCalculator {
     public static int sum(String input) {
+        Iterator<String> numbers = FluentIterable.from(Arrays.asList(input.split("")))
+                .filter(
+                        new Predicate<String>() {
+                            public boolean apply(String s) {
+                                return !s.equals(",");
+                            }
+                        }
+                )
+                .filter(
+                        new Predicate<String>() {
+                            public boolean apply(String s) {
+                                return !s.equals("\n");
+                            }
+                        }
+                )
+                .iterator();
+        System.out.println("Expression: " +
+                Joiner.on("+").join(numbers));
+
         if (input.length() == 0) {
             return 0;
         }
