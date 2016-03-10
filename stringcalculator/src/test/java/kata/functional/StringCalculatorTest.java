471bd614d6f8a556d1f79e136f205f4baef02d0b
Ben Wu <wubin28@gmail.com>
2016-03-10 13:13:03 +0800

    Wrote intent code.

--- stringcalculator/src/test/java/kata/functional/StringCalculatorTest.java
+++ stringcalculator/src/test/java/kata/functional/StringCalculatorTest.java
@@ -6,11 +6,10 @@ import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
     @Test
-    public void should() {
-        assertEquals(1, 1);
+    public void should_return_0_for_an_empty_string() {
+        assertEquals(0, StringCalculator.sum(""));
     }
 
-    // TODO: should_return_0_for_an_empty_string
     // TODO: should_return_the_number_itself_for_one_number
     // TODO: should_return_the_sum_for_two_numbers
 
